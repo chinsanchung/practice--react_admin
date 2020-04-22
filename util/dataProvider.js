b749c439bd3c450f7cdec6c725273614f36f7f40
Chinsan Chung <33484830+chinsanchung@users.noreply.github.com>
2020-04-22 19:01:23 +0900

    update: dataProvider

--- util/dataProvider.js
+++ util/dataProvider.js
@@ -1,93 +1,194 @@
-import { fetchUtils } from "react-admin";
+import { stringify } from "query-string";
+import {
+  fetchUtils,
+  GET_LIST,
+  GET_ONE,
+  GET_MANY,
+  GET_MANY_REFERENCE,
+  CREATE,
+  UPDATE,
+  UPDATE_MANY,
+  DELETE,
+  DELETE_MANY,
+} from "react-admin";
 
-const apiUrl = "https://my.api.com/";
-const httpClient = fetchUtils.fetchJson;
+/**
+ * Maps react-admin queries to a simple REST API
+ *
+ * The REST dialect is similar to the one of FakeRest
+ * @see https://github.com/marmelab/FakeRest
+ * @example
+ * GET_LIST     => GET http://my.api.url/posts?sort=['title','ASC']&range=[0, 24]
+ * GET_ONE      => GET http://my.api.url/posts/123
+ * GET_MANY     => GET http://my.api.url/posts?filter={ids:[123,456,789]}
+ * UPDATE       => PUT http://my.api.url/posts/123
+ * CREATE       => POST http://my.api.url/posts
+ * DELETE       => DELETE http://my.api.url/posts/123
+ */
+export default (apiUrl, httpClient = fetchUtils.fetchJson) => {
+  /**
+   * @param {String} type One of the constants appearing at the top if this file, e.g. 'UPDATE'
+   * @param {String} resource Name of the resource to fetch, e.g. 'posts'
+   * @param {Object} params The data request params, depending on the type
+   * @returns {Object} { url, options } The HTTP request parameters
+   */
+  const convertDataRequestToHTTP = (type, resource, params) => {
+    console.log("DataProvider_RequestToHTTP");
+    let url = "";
+    const options = {};
+    switch (type) {
+      case GET_LIST: {
+        console.log("DataProvider_Request_GET_LIST");
+        const { page, perPage } = params.pagination;
+        const { field, order } = params.sort;
+        const query = {
+          sort: JSON.stringify([field, order]),
+          pagination: JSON.stringify([page, perPage]),
+          filter: JSON.stringify(params.filter),
+        };
+        url = `${apiUrl}/${resource}?${stringify(query)}`;
+        break;
+      }
+      case GET_ONE:
+        // CUSTOM: url 에 show 를 추가
+        url = `${apiUrl}/${resource}/${params.id}/show`;
+        console.log("DataProvider_Request_GET_ONE ", url);
+        console.log(apiUrl);
+        console.log(resource);
+        console.log(params.id);
+        // console.log("url");
+        break;
+      case GET_MANY: {
+        const query = {
+          filter: JSON.stringify({ id: params.ids }),
+        };
+        url = `${apiUrl}/${resource}?${stringify(query)}`;
+        break;
+      }
+      case GET_MANY_REFERENCE: {
+        const { page, perPage } = params.pagination;
+        const { field, order } = params.sort;
+        const query = {
+          sort: JSON.stringify([field, order]),
+          range: JSON.stringify([(page - 1) * perPage, page * perPage - 1]),
+          filter: JSON.stringify({
+            ...params.filter,
+            [params.target]: params.id,
+          }),
+        };
+        url = `${apiUrl}/${resource}?${stringify(query)}`;
+        break;
+      }
+      case UPDATE:
+        console.log("DataProvider_Request_UPDATE");
+        url = `${apiUrl}/${resource}/${params.id}`;
+        options.method = "PUT";
+        options.body = JSON.stringify(params.data);
+        console.log(params.data);
+        break;
+      case CREATE:
+        console.log("DataProvider_Request_CREATE");
+        url = `${apiUrl}/${resource}`;
+        options.method = "POST";
+        options.body = JSON.stringify(params.data);
+        console.log(params.data);
+        break;
+      case DELETE:
+        url = `${apiUrl}/${resource}/${params.id}`;
+        options.method = "DELETE";
+        break;
+      default:
+        console.log("DataProvider_Request_default");
+        throw new Error(`Unsupported fetch action type ${type}`);
+    }
+    console.log({ Request: url });
+    return { url, options };
+  };
 
-export default {
-  getList: (resource, params) => {
-    const { page, perPage } = params.pagination;
-    const { field, order } = params.sort;
-    const query = {
-      sort: JSON.stringify([field, order]),
-      range: JSON.stringify([(page - 1) * perPage, page * perPage - 1]),
-      filter: JSON.stringify(params.filter),
-    };
-    const url = `${apiUrl}/${resource}?${stringify(query)}`;
+  /**
+   * @param {Object} response HTTP response from fetch()
+   * @param {String} type One of the constants appearing at the top if this file, e.g. 'UPDATE'
+   * @param {String} resource Name of the resource to fetch, e.g. 'posts'
+   * @param {Object} params The data request params, depending on the type
+   * @returns {Object} Data response
+   */
+  const convertHTTPResponse = (response, type, resource, params) => {
+    console.log("DataProvider_Response");
+    const { headers, json } = response;
+    const json_v = JSON.parse(
+      JSON.stringify(json).split('"_id":').join('"id":'),
+    );
 
-    return httpClient(url).then(({ headers, json }) => ({
-      data: json,
-      total: parseInt(headers.get("content-range").split("/").pop(), 10),
-    }));
-  },
+    switch (type) {
+      case GET_LIST:
+        console.log("DataProvider_Response_GET_LIST");
+        console.log(json_v);
+        return {
+          data: json_v,
+          total: parseInt(headers.get("Content-Range").split("/").pop(), 10),
+        };
+      case GET_MANY_REFERENCE:
+        if (!headers.has("Content-Range")) {
+          throw new Error(
+            "The Content-Range header is missing in the HTTP Response. The simple REST data provider expects responses for lists of resources to contain this header with the total number of results to build the pagination. If you are using CORS, did you declare Content-Range in the Access-Control-Expose-Headers header?",
+          );
+        }
+        return {
+          data: json_v,
+          total: parseInt(headers.get("Content-Range").split("/").pop(), 10),
+        };
+      case UPDATE:
+        console.log("DataProvider_Response_UPDATE");
+        return { data: json_v };
+      case CREATE:
+        console.log("DataProvider_Response_CREATE");
+        return { data: json_v };
+      case DELETE_MANY:
+        return { data: json_v || [] };
+      default:
+        console.log("DataProvider_Response_default");
+        return { data: json_v };
+    }
+  };
 
-  getOne: (resource, params) =>
-    httpClient(`${apiUrl}/${resource}/${params.id}`).then(({ json }) => ({
-      data: json,
-    })),
+  /**
+   * @param {string} type Request type, e.g GET_LIST
+   * @param {string} resource Resource name, e.g. "posts"
+   * @param {Object} payload Request parameters. Depends on the request type
+   * @returns {Promise} the Promise for a data response
+   */
+  return (type, resource, params) => {
+    // simple-rest doesn't handle filters on UPDATE route, so we fallback to calling UPDATE n times instead
+    if (type === UPDATE_MANY) {
+      return Promise.all(
+        params.ids.map((id) =>
+          httpClient(`${apiUrl}/${resource}/${id}`, {
+            method: "PUT",
+            body: JSON.stringify(params.data),
+          }),
+        ),
+      ).then((responses) => ({
+        data: responses.map((response) => response.json),
+      }));
+    }
+    // simple-rest doesn't handle filters on DELETE route, so we fallback to calling DELETE n times instead
+    if (type === DELETE_MANY) {
+      return Promise.all(
+        params.ids.map((id) =>
+          httpClient(`${apiUrl}/${resource}/${id}`, {
+            method: "DELETE",
+          }),
+        ),
+      ).then((responses) => ({
+        data: responses.map((response) => response.json),
+      }));
+    }
 
-  getMany: (resource, params) => {
-    const query = {
-      filter: JSON.stringify({ id: params.ids }),
-    };
-    const url = `${apiUrl}/${resource}?${stringify(query)}`;
-    return httpClient(url).then(({ json }) => ({ data: json }));
-  },
+    const { url, options } = convertDataRequestToHTTP(type, resource, params);
 
-  getManyReference: (resource, params) => {
-    const { page, perPage } = params.pagination;
-    const { field, order } = params.sort;
-    const query = {
-      sort: JSON.stringify([field, order]),
-      range: JSON.stringify([(page - 1) * perPage, page * perPage - 1]),
-      filter: JSON.stringify({
-        ...params.filter,
-        [params.target]: params.id,
-      }),
-    };
-    const url = `${apiUrl}/${resource}?${stringify(query)}`;
-
-    return httpClient(url).then(({ headers, json }) => ({
-      data: json,
-      total: parseInt(headers.get("content-range").split("/").pop(), 10),
-    }));
-  },
-
-  update: (resource, params) =>
-    httpClient(`${apiUrl}/${resource}/${params.id}`, {
-      method: "PUT",
-      body: JSON.stringify(params.data),
-    }).then(({ json }) => ({ data: json })),
-
-  updateMany: (resource, params) => {
-    const query = {
-      filter: JSON.stringify({ id: params.ids }),
-    };
-    return httpClient(`${apiUrl}/${resource}?${stringify(query)}`, {
-      method: "PUT",
-      body: JSON.stringify(params.data),
-    }).then(({ json }) => ({ data: json }));
-  },
-
-  create: (resource, params) =>
-    httpClient(`${apiUrl}/${resource}`, {
-      method: "POST",
-      body: JSON.stringify(params.data),
-    }).then(({ json }) => ({
-      data: { ...params.data, id: json.id },
-    })),
-
-  delete: (resource, params) =>
-    httpClient(`${apiUrl}/${resource}/${params.id}`, {
-      method: "DELETE",
-    }).then(({ json }) => ({ data: json })),
-
-  deleteMany: (resource, params) => {
-    const query = {
-      filter: JSON.stringify({ id: params.ids }),
-    };
-    return httpClient(`${apiUrl}/${resource}?${stringify(query)}`, {
-      method: "DELETE",
-      body: JSON.stringify(params.data),
-    }).then(({ json }) => ({ data: json }));
-  },
+    return httpClient(url, options).then((response) => {
+      console.log(response);
+      return convertHTTPResponse(response, type, resource, params);
+    });
+  };
 };
