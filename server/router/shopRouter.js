5e3d43ec4765349d32cbbd16f144a65d1b163b3f
Chinsan Chung <33484830+chinsanchung@users.noreply.github.com>
2020-04-22 19:01:13 +0900

    update: 라우트들을 미들웨어로 만듦

--- server/router/shopRouter.js
+++ server/router/shopRouter.js
@@ -1,9 +1,19 @@
 import express from "express";
 import routes from "../routes";
-import { getShopList } from "../controllers/shopController";
+import {
+  GET_LIST,
+  CREATE,
+  GET_ONE,
+  DELETE,
+  UPDATE,
+} from "../controllers/shopController";
 
 const shopRouter = express.Router();
 
-shopRouter.get(routes.shops, getShopList);
+shopRouter.get("", GET_LIST);
+shopRouter.get(`${routes.id_url}/show`, GET_ONE);
+shopRouter.post("", CREATE);
+shopRouter.delete(routes.id_url, DELETE);
+shopRouter.put(routes.id_url, UPDATE);
 
 export default shopRouter;
