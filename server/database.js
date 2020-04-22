77d9e1698676b938c104c15fcc7fd69f7d2cd158
Chinsan Chung <33484830+chinsanchung@users.noreply.github.com>
2020-04-22 19:00:35 +0900

    update: 이름 변경, mongoose 로 DB 연결

---
+++ server/database.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const URL = "mongodb://localhost:27017/golmok";
+// const URL =
+//   "mongodb+srv://moon:mmmm1259@wesavercluster-w2rvc.mongodb.net/test?retryWrites=true&w=majority";
+
+mongoose.connect(URL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () =>
+  console.log(`데이터베이스 연결: ${process.env.MONGO_URL}`);
+const handleError = (error) => console.log(`데이터베이스 연결 에러: ${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
