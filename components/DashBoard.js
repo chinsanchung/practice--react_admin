d885f882da9672443558fea5d2ebd7881005c3bd
Chinsan Chung <33484830+chinsanchung@users.noreply.github.com>
2020-04-23 18:38:04 +0900

    update: Title 컴포넌트 추가

--- components/DashBoard.js
+++ components/DashBoard.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Title } from "react-admin";
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 
 function DashBoard() {
   return (
     <Card>
+      <Title title="골목골목" />
       <CardHeader title="골목골목 관리자 페이지에 오신 것을 환영합니다." />
     </Card>
   );
