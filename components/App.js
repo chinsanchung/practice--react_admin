f38575a288a8e9fbf782307de5e6ba77109879c3
Chinsan Chung <33484830+chinsanchung@users.noreply.github.com>
2020-04-23 18:36:29 +0900

    update: 각 부문들을 App 하위 컴포넌트로 추가

--- components/App.js
+++ components/App.js
@@ -1,27 +1,25 @@
-import React, { useEffect } from "react";
-import { Admin, Resource, ListGuesser } from "react-admin";
+import React from "react";
+import { Admin, Resource } from "react-admin";
 import authProvider from "../util/authProvider";
-import fakeData from "../server/fakeData";
-import fakeDataProvider from "ra-data-fakerest";
 import dataProvider from "../util/dataProvider";
 import DashBoard from "./DashBoard";
-// Shop
+
 import shopComponents from "./Shop/index";
-// Members
-import PersonIcon from "@material-ui/icons/Person";
-// Events
-import EventIcon from "@material-ui/icons/Event";
-// Announces
-import AnnouncementIcon from "@material-ui/icons/Announcement";
+import memberComponents from "./Member/index";
+import eventComponents from "./Event/index";
+import announceComponents from "./Announce";
 
 function App() {
-  useEffect(() => {}, []);
   return (
     <Admin
+      authProvider={authProvider}
       dashboard={DashBoard}
       dataProvider={dataProvider("http://localhost:4000")}
     >
       <Resource name="Shops" {...shopComponents} />
+      <Resource name="Members" {...memberComponents} />
+      <Resource name="Events" {...eventComponents} />
+      <Resource name="Announces" {...announceComponents} />
     </Admin>
   );
 }
